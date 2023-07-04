022176a6e92620f2b9422a7af51b0370c3ccbe2f
ulug_beey_03 <ahmadjonovulugbek.o3@gmail.com>
2023-07-04 20:02:42 +0500

    init: first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const username = document.querySelector('.username')
+const password = document.querySelector('.password')
+const passwordBtn = document.querySelector('.password-button')
+const face = document.querySelector('.face')
+const hands = document.querySelectorAll('.hand')
+const tongue = document.querySelector('.tongue')
+
+username.addEventListener('focus', faceRotation)
+username.addEventListener('input', faceRotation)
+username.addEventListener('blur', () => face.style.transform = "rotate(0deg)") 
+
+function faceRotation() {
+    const value = username.value.length
+
+    if (value < 40) {
+        face.style.transform = `rotate(${20 - value}deg)`
+    }
+}
+
+password.addEventListener('focus', () => {
+    tongue.classList.remove('breath')
+    hands.forEach(hand => hand.classList.add('hide'))
+})
+
+password.addEventListener('blur', () => {
+    tongue.classList.add('breath')
+    hands.forEach(hand => hand.classList.remove('hide'))
+})
+
+passwordBtn.addEventListener('mousedown', () => {
+    tongue.classList.remove('breathe')
+    hands.forEach(hand => hand.classList.add('peek'))
+    password.type ='text'
+})
+
+passwordBtn.addEventListener('mouseup', () => {
+    tongue.classList.add('breath')
+    hands.forEach(hand => hand.classList.remove('peek'))
+    password.type ='password'
+})
